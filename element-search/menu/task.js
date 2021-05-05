591d27561395b62c7d53017c2745b37744db2331
keka3avr <kekazavr228@gmail.com>
2021-05-06 00:00:45 +1100

    fix homework2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,64 +1,30 @@
 "use strict";
-function menuAction() {
-    const linksArr = Array.from(document.getElementsByClassName("menu__link"));
-    let activeTab = null;
-    let activeMenu = null;
-    let menuChanged = false;
+const linksArr = Array.from(document.getElementsByClassName("menu__link"));
+let activeTab = null;
+let activeMenu = null;
 
-    function setMenuListener() {
-        const menuArr = Array.from(document.getElementsByClassName("menu-main"));
-        menuArr.forEach(element => 
+function setTabListener() {
+    linksArr.forEach(element => {
+        const sibling = element.nextElementSibling;
+        if (sibling) {
             element.onclick = function() {
-                if (activeMenu != null && activeMenu != element) {
-                    menuChanged == true;    
-                }
-                activeMenu = element;
-            }
-        )
-    }
-
-    function setTabListener() {
-        linksArr.forEach(element => {
-            const sibling = element.nextElementSibling;
-            if (sibling) {
-                element.onclick = function() {
-                    showTab();
-                    checkActiveTab(sibling);
-                    return false;
-                }
-            }
-            function showTab() {
-                sibling.className = "menu menu_active";
-            }   
-        })
-    }
-
-    function checkActiveTab(clickedTab) {
-        if (!menuChanged) {
-            if (!activeTab) activeTab = clickedTab;
-            else if (activeTab != clickedTab) {
-                hideTab()
-                activeTab = clickedTab;
-            }
-            else if (activeTab == clickedTab) {
-                hideTab();
-                activeTab = null;
+                sibling.classList.add("menu_active");
+                checkActiveTab(sibling);
+                return false;
             }
         }
-        else {
-            activeTab = clickedTab;
-            menuChanged = false;
-        }
-
-        function hideTab() {
-            activeTab.className = "menu menu_sub";
-        }
-    }
-
-
-    setMenuListener();
-    setTabListener();
-    
+    })
 }
 
-menuAction();
+function checkActiveTab(clickedTab) {
+    if (!activeTab) activeTab = clickedTab;
+    else {
+        activeTab.className = "menu menu_sub";
+        activeTab = activeTab != clickedTab ? clickedTab : null;
+    }
+}
+
+
+setTabListener();
+    
+
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,80 +1,76 @@
-function sliderWork() {
-    const images = Array.from(document.getElementsByClassName("slider__item"));
-    const dots = Array.from(document.getElementsByClassName("slider__dot"));
-    let activeImg, activeDot = null;
 
-    function getAndHideActiveElements() {
-        activeImg = document.getElementsByClassName("slider__item_active")[0];
-        activeDot = document.getElementsByClassName("slider__dot_active")[0];
-        activeImg.className = "slider__item";
-        activeDot.className = "slider__dot";
-    }
+const images = Array.from(document.getElementsByClassName("slider__item"));
+const dots = Array.from(document.getElementsByClassName("slider__dot"));
+let activeImg, activeDot = null;
 
-    function showElements(img, dot) {
-        img.className = "slider__item slider__item_active";
-        dot.className = "slider__dot slider__dot_active";
-    }
-
-    // <------------- Start Arrows Listener -----------> // 
-    function setArrowsListener() {
-        const arrows = Array.from(document.getElementsByClassName("slider__arrow"));
-
-        arrows.forEach(element =>  
-            element.onclick = () => arrowClick(element)
-        )    
-    }
-
-    function arrowClick(arrow) {
-
-        function ifMoveLeft() {
-            if (arrow.className.includes("slider__arrow_prev")) {
-                let prevImg = activeImg.previousElementSibling;
-                let prevDot = activeDot.previousElementSibling;
-
-                if (!prevImg) {
-                    prevImg = images[images.length - 1];
-                    prevDot = dots[dots.length - 1];
-                }
-                showElements(prevImg, prevDot);
-            }
-        }
-
-        function ifMoveRight() {
-            if (arrow.className.includes("slider__arrow_next")) {
-                let nextImg = activeImg.nextElementSibling;
-                let nextDot = activeDot.nextElementSibling;
-
-                if (!nextImg) {
-                    nextImg = images[0];
-                    nextDot = dots[0]
-                }
-                showElements(nextImg, nextDot);
-            }
-        }
-
-        getAndHideActiveElements();
-        ifMoveLeft();
-        ifMoveRight();
-    }
-
-    setArrowsListener();
-    // <------------- End Arrows Listener -----------> // 
-
-
-    // <------------- Start Dots Listener -----------> // 
-    function setDotsListener() {
-        dots.forEach((element, index) => 
-            element.onclick = () => {
-                const equalImg = images[index];
-
-                getAndHideActiveElements();
-                showElements(equalImg, element);
-            }
-        )
-    }
-
-    setDotsListener();
-    // <------------- End Dots Listener -----------> // 
+function hideActiveSlide() {
+    activeImg = images.find(element => element.className.includes("slider__item_active"));
+    activeDot = dots.find(element => element.className.includes("slider__dot_active"));
+    activeImg.className = "slider__item";
+    activeDot.className = "slider__dot";
 }
 
-sliderWork();
+function showSlide(img, dot) {
+    img.className = "slider__item slider__item_active";
+    dot.className = "slider__dot slider__dot_active";
+}
+
+// <------------- Start Arrows Listener -----------> // 
+function setArrowsListener() {
+    const arrows = Array.from(document.getElementsByClassName("slider__arrow"));
+
+    arrows.forEach(element =>  
+        element.onclick = () => arrowClick(element)
+    )    
+}
+
+function arrowClick(arrow) {
+    hideActiveSlide();
+    if (arrow.className.includes("slider__arrow_prev")) {
+        moveLeft();
+    }
+    else moveRight();
+}
+
+function moveLeft() {
+    let prevImg = activeImg.previousElementSibling;
+    let prevDot = activeDot.previousElementSibling;
+
+    if (!prevImg) {
+        prevImg = images[images.length - 1];
+        prevDot = dots[dots.length - 1];
+    }
+    showSlide(prevImg, prevDot);    
+}
+
+function moveRight() {
+    let nextImg = activeImg.nextElementSibling;
+    let nextDot = activeDot.nextElementSibling;
+
+    if (!nextImg) {
+        nextImg = images[0];
+        nextDot = dots[0]
+    }
+    showSlide(nextImg, nextDot);
+}
+
+setArrowsListener();
+// <------------- End Arrows Listener -----------> // 
+
+
+// <------------- Start Dots Listener -----------> // 
+function setDotsListener() {
+    dots.forEach((element, index) => 
+        element.onclick = () => {
+            const equalImg = images[index];
+
+            hideActiveSlide();
+            showSlide(equalImg, element);
+        }
+    )
+}
+
+setDotsListener();
+// <------------- End Dots Listener -----------> // 
+
+
